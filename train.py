f4139412548ff208e68f3d7a3760414803f3e198
caoj <caojics@outlook.com>
2024-05-03 09:29:33 +0800

    调整预训练超参

--- train.py
+++ train.py
@@ -41,8 +41,8 @@ if __name__ == "__main__":
     parser.add_argument("--gnn_head_num", default=2, type=int)
     parser.add_argument("--gnn", default="gat", type=str)
 
-    parser.add_argument("--lr", type=float, default=5e-4)
-    parser.add_argument("--weight_decay", type=float, default=1e-4)
+    parser.add_argument("--lr", type=float, default=1e-3)
+    parser.add_argument("--weight_decay", type=float, default=1e-5)
     parser.add_argument("--lr_patience", type=float, default=2)
     parser.add_argument("--lr_decay_ratio", type=float, default=1e-2)
     parser.add_argument("--early_stop_lr", type=float, default=1e-6)
@@ -56,7 +56,7 @@ if __name__ == "__main__":
     parser.add_argument("--bias", default=False, type=bool)
 
     # optimization settings
-    parser.add_argument("--grad_clip", default=1.0, type=float)
+    parser.add_argument("--grad_clip", default=5.0, type=float)
     parser.add_argument("--eval_only", default=False, type=bool)
     parser.add_argument("--eval_interval", default="10", type=int)
 
@@ -101,7 +101,7 @@ if __name__ == "__main__":
         adj_dense = np.load(adj_no_isolate_file)
     else:
         for i in range(len(adj_dense)):
-            adj_dense[i][i] = 1
+            # adj_dense[i][i] = 1
             assert adj_dense[i].sum() > 0
             if adj_dense[i].sum() == 0:
                 adj_dense[np.random.randint(0, len(adj_dense), 1)[0]] = 1
@@ -177,16 +177,16 @@ if __name__ == "__main__":
             device,
         )
         for batch in tqdm(tra_loader, desc="train transformer"):
-            with args.ctx:
-                optimizer.zero_grad(set_to_none=True)
-                _, _, loss = model(
-                    batch[0], batch[1], batch[2], batch[3], batch[4], batch[5]
-                )
-                loss.backward()
-                train_total_loss += loss.item()
-                if args.grad_clip != 0.0:
-                    torch.nn.utils.clip_grad_norm_(model.parameters(), args.grad_clip)
-                optimizer.step()
+            # with args.ctx:
+            optimizer.zero_grad(set_to_none=True)
+            _, _, loss = model(
+                batch[0], batch[1], batch[2], batch[3], batch[4], batch[5]
+            )
+            loss.backward()
+            train_total_loss += loss.item()
+            if args.grad_clip != 0.0:
+                torch.nn.utils.clip_grad_norm_(model.parameters(), args.grad_clip)
+            optimizer.step()
             start_i = start_i + args.batch_size
         logger.info(
             "epoch {}: train_loss {:.6f}".format(
@@ -197,17 +197,17 @@ if __name__ == "__main__":
             model.eval()
             val_hit, val_cnt = 0, 0
             start_i = 0
-            for batch in tqdm(tra_loader, desc="valid transformer"):
-                with args.ctx:
-                    logits_masked, _, _ = model(
-                        batch[0], batch[1], batch[2], batch[3], batch[4], batch[5]
-                    )
+            for batch in tqdm(val_loader, desc="valid transformer"):
+                # with args.ctx:
+                logits_masked, _, _ = model(
+                    batch[0], batch[1], batch[2], batch[3], batch[4], batch[5]
+                )
                 value, index = torch.topk(logits_masked, 1, dim=-1)
                 val_hit += (index.squeeze(-1) == batch[1]).sum()
                 val_cnt += (batch[1] != -1).sum()
                 start_i = start_i + args.batch_size
             avg_ac = val_hit / val_cnt
-            logger.info("epoch {}: eval_top1_ac {:.6f}".format(epoch, avg_ac))
+            logger.info("epoch {}: eval_top1_ac {:.6f} ({} / {})".format(epoch, avg_ac, val_hit, val_cnt))
             if avg_ac > best_val_avg_acc_top1:
                 best_val_avg_acc_top1 = avg_ac
                 ckpt = {
